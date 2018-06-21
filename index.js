2ecf42f2c53a310640baddc3d04dcfadfef56b42
sophiesmith6 <18smiths@corjesu.org>
2018-06-21 21:15:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i< lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString();
+  }
+}
